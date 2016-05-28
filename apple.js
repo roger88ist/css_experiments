f33576847ea337bc6b27b2cc8343afd9903b1757
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-05-28 11:27:55 -0400

    added the desired effect with the background but still need to target only the div that gets the mouse event.

--- apple.js
+++ apple.js
@@ -2,8 +2,10 @@ $(function() {
   $("p").hide();
   $("#item").mouseenter(function(){
   	$("p").show();
+  	$("#canvas").css("background", "rgba(118, 118, 118, 0.3)")
   });
   $("#item").mouseleave(function(){
   	$("p").hide();
+  	$("#canvas").css("background", "none");
   });
 });
