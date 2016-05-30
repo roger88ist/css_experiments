78c4516337b20c7190e03998454621d646e135e1
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-05-30 01:36:58 -0400

    So if I target them seperately the effect that I want happens. The issue I am having though is that everytime I mouseenter one div, the other div shifts down a little until I mouseleave and then the other div shifts up back into place. Trying to resolve this issue.

--- apple.js
+++ apple.js
@@ -1,11 +1,19 @@
 $(function() {
   $("p").hide();
-  $("#item").mouseenter(function(){
-  	$("p").show();
-  	$("#canvas").css("background", "rgba(118, 118, 118, 0.3)")
+  $(".first").mouseenter(function(){
+  	$(".first p").show();
+  	$(".first #canvas").css("background", "rgba(118, 118, 118, 0.3)")
   });
-  $("#item").mouseleave(function(){
-  	$("p").hide();
-  	$("#canvas").css("background", "none");
+  $(".first").mouseleave(function(){
+  	$(".first p").hide();
+  	$(".first #canvas").css("background", "none");
+  });
+  $(".second").mouseenter(function(){
+  	$(".second p").show();
+  	$(".second #canvas").css("background", "rgba(118, 118, 118, 0.3)")
+  });
+  $(".second").mouseleave(function(){
+  	$(".second p").hide();
+  	$(".second #canvas").css("background", "none");
   });
 });
